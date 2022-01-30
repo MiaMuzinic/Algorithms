8a43caf6c3265ac50bbfaead912d5ac24ff19317
MiaMuzinic <muzinicmia@gmail.com>
2022-01-30 23:41:16 +0100

    Add project files.

---
+++ WindowsFormsApp1/Class1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Class1
+    {
+        // 1. BRUTE FORCE   https://www.geeksforgeeks.org/naive-algorithm-for-pattern-searching/
+        public bool BruteForce(string uzorak, string tekst)
+        {
+            uzorak = uzorak.ToLower();
+            tekst = tekst.ToLower();
+            int velicina_uzorka = uzorak.Length;
+            int velicina_teksta = tekst.Length;
+
+            for (int i = 0; i <= velicina_teksta - velicina_uzorka; i++)
+            {
+                int j;
+                for (j = 0; j < velicina_uzorka; j++)
+                {
+                    if (tekst[i + j] != uzorak[j])
+                    {
+                        break;
+                    }
+                }
+                if (j == velicina_uzorka)
+                {
+                    return true; 
+                }
+            }
+            return false;
+        }
+
+
+        // 2. Boyer-Moore-Simple-Search  https://www.baeldung.com/java-full-text-search-algorithms
+        public bool BoyerMooreSimpleSearch(string uzorak, string tekst)
+        {
+            uzorak = uzorak.ToLower();
+            tekst = tekst.ToLower();
+            int velicina_uzorka = uzorak.Length;
+            int velicina_teksta = tekst.Length;
+
+            int i = 0, j = 0;
+
+            while ((i + velicina_uzorka) <= velicina_teksta)
+            {
+                j = velicina_uzorka - 1;
+
+                while (tekst[i + j] == uzorak[j])
+                {
+                    j--;
+                    if (j < 0)
+                        return true;
+                }
+                i++;
+            }
+            return false;
+        }
+
+
+        // 3. RABIN-KARP https://www.geeksforgeeks.org/rabin-karp-algorithm-for-pattern-searching/
+        public bool RabinKarp(string uzorak, string tekst)
+        {
+            tekst = tekst.ToLower();
+            uzorak = uzorak.ToLower();
+            ulong p = 0;
+            ulong t = 0;
+            ulong q = 100007;
+            ulong ASCII = 256;
+            for (int i = 0; i < uzorak.Length; i++)
+            {
+                try
+                {
+                    p = (p * ASCII + (ulong)tekst[i]) % q;
+                    t = (t * ASCII + (ulong)uzorak[i]) % q;
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    return false;
+                }
+            }
+            if (p == t)
+            {
+                return true;
+            }
+            ulong h = 1;
+            for (int i = 1; i <= uzorak.Length - 1; i++)
+                h = (h * ASCII) % q;
+
+            for (int j = 1; j <= tekst.Length - uzorak.Length; j++)
+            {
+                p = (p + q - h * (ulong)tekst[j - 1] % q) % q;
+                p = (p * ASCII + (ulong)tekst[j + uzorak.Length - 1]) % q;
+                if (p == t)
+                {
+                    if (tekst.Substring(j, uzorak.Length) == uzorak)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+
+        // 4. KNUTH–MORRIS–PRATT   https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
+        public bool KnutMorrisPratt(string uzorak, string tekst)
+        {
+            uzorak = uzorak.ToLower();
+            tekst = tekst.ToLower();
+            int velicina_uzorka = uzorak.Length;
+            int velicina_teksta = tekst.Length;
+
+            int[] longest_prefix_suffix = new int[velicina_uzorka];
+
+            int j = 0; 
+            computeLPSArray(uzorak, velicina_uzorka, ref longest_prefix_suffix);
+
+            int i = 0;
+            while (i < velicina_teksta)
+            {
+                if (uzorak[j] == tekst[i])
+                {
+                    j++;
+                    i++;
+                }
+                if (j == velicina_uzorka)
+                {
+                    return true;
+                }
+                else if (i < velicina_teksta && uzorak[j] != tekst[i])
+                {
+                    if (j != 0)
+                        j = longest_prefix_suffix[j - 1];
+                    else
+                        i = i + 1;
+                }
+            }
+            return false;
+        }
+        public void computeLPSArray(string pat, int M, ref int[] lps)
+        {
+            int len = 0;
+            int i = 1;
+            lps[0] = 0; 
+
+            while (i < M)
+            {
+                if (pat[i] == pat[len])
+                {
+                    len++;
+                    lps[i] = len;
+                    i++;
+                }
+                else
+                {
+                    if (len != 0)
+                    {
+                        len = lps[len - 1];
+                    }
+                    else
+                    {
+                        lps[i] = len;
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] recenice;
+        string odabranitekst = "";
+        Class1 c = new Class1();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+         
+            if (listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Niste odabrali tekst!");
+                return;
+            }
+            else
+            {
+                odabranitekst = listBox1.SelectedItem.ToString() + ".txt";
+            }
+
+            textBox1.Enabled = true;
+
+            char[] krajevirecenica = new char[] { '.', '!', '?'}; 
+            using (StreamReader sr = File.OpenText(odabranitekst)) 
+            {
+                string tekst = sr.ReadToEnd();
+                recenice = tekst.Split(krajevirecenica);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+          
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+        private void Oznacavanje(string uzorak)
+        {
+            int da = 0;
+            int indeks=0;
+            string t = richTextBox1.Text;
+            richTextBox1.Text = "";
+            richTextBox1.Text = t;
+            while (indeks < richTextBox1.Text.LastIndexOf(uzorak))
+            {
+                richTextBox1.Find(uzorak, indeks, richTextBox1.TextLength, RichTextBoxFinds.None);               
+                richTextBox1.SelectionColor = Color.GreenYellow;
+                da += 1;
+                indeks = richTextBox1.Text.IndexOf(uzorak, indeks) + 1;
+            }
+            if (da>=10)
+            {
+                richTextBox1.AppendText("DOBIVENI PODACI NAKON OBAVLJENOG ALGORITMA SU:" +"\n");
+                richTextBox1.AppendText("Broj pojavljivanja upisane riječi je ");
+                richTextBox1.AppendText(da.ToString());
+                richTextBox1.AppendText(" i ona se često pojavljuje u odabranom tekstu.");            
+            }
+            else
+            {
+                richTextBox1.AppendText("DOBIVENI PODACI NAKON OBAVLJENOG ALGORITMA SU:" + "\n");
+                richTextBox1.AppendText("Broj pojavljivanja upisane riječi je ");
+                richTextBox1.AppendText(da.ToString());
+                richTextBox1.AppendText(" i ona se ne pojavljuje toliko često u odabranom tekstu.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            string uzorak = textBox1.Text;
+            if (uzorak == "")
+            {
+                return;
+            }
+            foreach (string item in recenice)
+            {
+                if (c.KnutMorrisPratt(uzorak, item))
+                {
+                    richTextBox1.AppendText(item + "\n\n");
+                }
+            }
+
+            if (richTextBox1.Text == "")
+            {
+                richTextBox1.AppendText("Nije pronaden trazeni uzorak!");
+            }
+            else
+            {
+                Oznacavanje(uzorak);
+            }
+
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            string uzorak = textBox1.Text;
+            if (uzorak == "")
+            {
+                return;
+            }
+
+            foreach (string item in recenice)
+            {
+                if (c.BruteForce(uzorak, item))
+                {
+                    richTextBox1.AppendText(item + "\n\n");
+                }
+            }
+
+            if (richTextBox1.Text == "")
+            {
+                richTextBox1.AppendText("Nije pronaden trazeni uzorak!");
+            }
+            else
+            {
+                Oznacavanje(uzorak);
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            string uzorak = textBox1.Text;
+            if (uzorak == "")
+            {
+                return;
+            }
+
+            foreach (string item in recenice)
+            {
+                if (c.BoyerMooreSimpleSearch(uzorak, item))
+                {
+                    richTextBox1.AppendText(item + "\n\n");
+                }
+            }
+
+            if (richTextBox1.Text == "")
+            {
+                richTextBox1.AppendText("Nije pronaden trazeni uzorak!");
+            }
+            else
+            {
+                Oznacavanje(uzorak);
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            string uzorak = textBox1.Text;
+            if (uzorak == "")
+            {
+                return;
+            }
+
+            foreach (string item in recenice)
+            {
+                if (c.RabinKarp(uzorak, item))
+                {
+                    richTextBox1.AppendText(item + "\n\n");
+                }
+            }
+
+            if (richTextBox1.Text == "")
+            {
+                richTextBox1.AppendText("Nije pronaden trazeni uzorak!");
+            }
+            else
+            {
+                Oznacavanje(uzorak);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            textBox1.Clear();
+            if (checkBox1.Checked==true)
+            {
+                checkBox1.Checked=false;
+            }
+            if (checkBox2.Checked == true)
+            {
+                checkBox2.Checked = false;
+            }
+            if (checkBox3.Checked == true)
+            {
+                checkBox3.Checked = false;
+            }
+            if (checkBox4.Checked == true)
+            {
+                checkBox4.Checked = false;
+            }
+            
+            listBox1.ClearSelected(); 
+            
+        }
+
+    }
+}
